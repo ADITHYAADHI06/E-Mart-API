4cea57b8bdde59aa578b8a3730cd6ba68fb010ec
ADITHYAADHI06 <adishettygu102@gmail.com>
2022-12-14 20:07:37 +0530

    adding stored API Data

---
+++ ProductsDB.js
@@ -0,0 +1,22 @@
+const connectDB = require("./db/conection");
+const Product = require("./models/products");
+
+require("dotenv").config();
+
+const ProductJson = require("./Products.json");
+
+
+const start = async () => {
+
+    try {
+        await connectDB(process.env.MONGODB_URL)
+        await Product.create(ProductJson)
+        console.log("success");
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+
+start();
---
+++ models/products.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose")
+const productSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: [true, "price must  be provided"]
+        },
+        featured: {
+            type: Boolean,
+            default: false
+        },
+        rating: {
+            type: Number,
+            default: 4.9,
+            required: [true, "rating must  be provided"]
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now()
+        },
+        company: {
+            type: String,
+            enum: {
+                values: ["apple", "samsung", "dell", "mi"],
+                message: `{value} is not supproted`
+            },
+        },
+    }
+)
+
+
+module.exports = mongoose.model("Product", productSchema)
