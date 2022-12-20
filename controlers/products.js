f7d87cd9d8f1b65db3e9a21acb7ed2d7451fd738
ADITHYAADHI06 <adishettygu102@gmail.com>
2022-12-20 20:07:15 +0530

    All functionalities Implemented for E-commerce Api using Nodejs

--- controlers/products.js
+++ controlers/products.js
@@ -1,8 +1,55 @@
 const Product = require("../models/products")
 
 const getAllProducts = async (req, res) => {
-    const myData = await Product.find({});
-    res.status(200).json({ myData })
+
+    const { company, name, featured, sort, select } = req.query;
+    // console.log("🚀 ~ file: products.js ~ line 5 ~ getAllProducts ~ sort", sort);
+    const queryObject = {};
+
+    if (company) {
+        queryObject.company = company;
+    }
+
+    if (featured) {
+        queryObject.featured = featured;
+    }
+
+    if (name) {
+        queryObject.name = { $regex: name, $options: "i" };
+    }
+
+    let apiData = Product.find(queryObject);
+
+    if (sort) {
+        let sortFix = sort.split(",").join(" ");
+        apiData = apiData.sort(sortFix);
+    }
+
+    if (select) {
+        //     // let selectFix = select.replace(",", " ");
+        let selectFix = select.split(",").join(" ");
+        apiData = apiData.select(selectFix);
+    }
+
+    let page = Number(req.query.page) || 1;
+    let limit = Number(req.query.limit) || 10;
+
+    let skip = (page - 1) * limit;
+
+    // page = 2;
+    // limit = 3;
+    // skip =  1 * 3 = 3
+
+    apiData = apiData.skip(skip).limit(limit);
+
+    // console.log(queryObject);
+    try {
+        const Products = await apiData;
+        res.status(200).json({ Products })
+    } catch (error) {
+        res.status(404).json("Bad request");
+        console.log(error)
+    }
 }
 
 
